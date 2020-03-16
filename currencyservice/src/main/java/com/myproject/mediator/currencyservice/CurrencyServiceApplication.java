7ddb36116824e3be2d3582f9efe7ebead47a93eb
TanyaEf <efimenkot@gmail.com>
2020-03-16 18:46:52 +0200

    Eureka client registration

--- currencyservice/src/main/java/com/myproject/mediator/currencyservice/CurrencyServiceApplication.java
+++ currencyservice/src/main/java/com/myproject/mediator/currencyservice/CurrencyServiceApplication.java
@@ -3,9 +3,11 @@ package com.myproject.mediator.currencyservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class CurrencyServiceApplication {
 
 	public static void main(String[] args) {
